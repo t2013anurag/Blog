2b86a666048f384f6ac7d9841bfc888362e4ea05
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-04-17 13:03:17 +0530

    Web Service for Login

---
+++ Blog/WebService1.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Blog
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public Boolean validateUser(string username, string password)
+        {
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\Anurag\\Documents\\Visual Studio 2013\\Projects\\Blog\\db\\Database2.mdb");
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand("select password from userdetails where username='" + username + "' and password='" + password + "'", conn);
+            OleDbDataReader dr = cmd.ExecuteReader();
+            return dr.Read();
+            conn.Close();
+        }
+    }
+}
