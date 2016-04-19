a7d32c69c296f0437a07cc13a921d560bfe679b2
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-04-19 21:35:06 +0530

    Update WebService1.asmx.cs

--- Blog/WebService1.asmx.cs
+++ Blog/WebService1.asmx.cs
@@ -8,14 +8,9 @@ using System.Data;
 
 namespace Blog
 {
-    /// <summary>
-    /// Summary description for WebService1
-    /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
-    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
     public class WebService1 : System.Web.Services.WebService
     {
 
